0f36e010769c0f63d684e87bb4718618766639a7
Mahmud256 <mahmud474849@gmail.com>
2023-11-08 02:49:43 +0600

    Correction Delete System

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Close the client when you finish or encounter an error
